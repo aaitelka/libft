9df742908c352ae6d32038a4dfca83128baf51e7
Halem <aaitelk@gmail.com>
2023-11-07 23:42:09 +0100

    create libft.h

---
+++ libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 23:18:09 by aaitelka          #+#    #+#             */
+/*   Updated: 2023/11/07 23:40:57 by aaitelka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+/**
+ *	Part 0ne.
+ * */
+
+int
+	ft_isalpha(int c);
+
+int
+	ft_isdigit(int c);
+
+int
+	ft_isalnum(int c);
+
+int
+	ft_isascii(int c);
+
+int
+	ft_isprint(int c);
+
+int
+	ft_toupper(int c);
+
+int
+	ft_tolower(int c);
+
+#endif
