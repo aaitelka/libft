eaab5b807d3b946e4e0f8f8460d33c7c09caee97
Halem <aaitelk@gmail.com>
2023-11-08 12:43:53 +0100

    create ft_strrchr.c and minify strchr.c code, and also i add prototype of strrchr in Makefile and libft.h

---
+++ ft_strrchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 12:01:38 by aaitelka          #+#    #+#             */
+/*   Updated: 2023/11/08 12:39:33 by aaitelka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	s += len;
+	while (*s != c && len--)
+		s--;	
+	if(*s == c)
+		return ((char *)s);
+	return (NULL);
+}
