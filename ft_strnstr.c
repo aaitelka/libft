47e2e95cabce7dd3451d9153f2f6a582e68a412c
Halem <aaitelk@gmail.com>
2023-11-13 12:02:09 +0100

    correct some issues in ft_strnstr.c function

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 14:06:31 by aaitelka          #+#    #+#             */
-/*   Updated: 2023/11/10 15:27:49 by aaitelka         ###   ########.fr       */
+/*   Updated: 2023/11/13 11:37:53 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	needle_len = ft_strlen(needle);
 	if (!*needle)
 		return ((char *)haystack);
-	while (*haystack && len--)
+	while (*haystack && len-- >= needle_len)
 	{
 		if (ft_strncmp(haystack, needle, needle_len) == 0)
 			return ((char *)haystack);
 		haystack++;
-	}
+		}
 	return (NULL);
 }
