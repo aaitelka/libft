44173a0701d0d7f33e8894eb7c7b3fe1a8d55779
Halem <aaitelk@gmail.com>
2023-11-14 12:55:15 +0100

    i fix some issues in ft_strtrim.c and ft_split.c functions, everything is good now.

--- ft_split.c
+++ ft_split.c
@@ -6,58 +6,73 @@
 /*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 20:18:35 by aaitelka          #+#    #+#             */
-/*   Updated: 2023/11/13 12:14:19 by aaitelka         ###   ########.fr       */
+/*   Updated: 2023/11/14 12:52:11 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_words(const char *s, char c)
+static int	count_words(const char *s, int c)
 {
-	static int	count;
+	int	is_c;
+	int	counter;
 
-	count = 0;
+	is_c = 1;
+	counter = 0;
 	while (*s)
 	{
-		while (*s == c)
-			s++;
-		if (*s && *s != c)
-			count++;
-		while (*s && *s != c)
-			s++;
+		if (*s == c)
+			is_c = 1;
+		else if (is_c && ++counter)
+			is_c = 0;
+		s++;
 	}
-	return (count);
+	return (counter);
 }
 
-char	**ft_split(const char *s, char c)
+static void	*free_me(char **str_arr, int i)
 {
-	char	**splited;
-	char	*trimmed;
-	char	**str_arr;
-	int		arr_size;
+	while (i--)
+		free(str_arr[i]);
+	free(str_arr);
+	return (NULL);
+}
+
+static char	**split(char **str_arr, char const *s, char c)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			len = 0;
+			while (s[len] && s[len] != c)
+				len++;
+			str_arr[i] = ft_substr(s, 0, len);
+			if (!str_arr[i])
+				return (free_me(str_arr, i));
+			s += len;
+			i++;
+		}
+	}
+	str_arr[i] = NULL;
+	return (str_arr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strs;
 
 	if (!s)
 		return (NULL);
-	trimmed = ft_strtrim(s, &c);
-	if (!trimmed)
+	strs = (char **)ft_calloc(count_words(s, c) + 1, sizeof(char *));
+	if (!strs)
 		return (NULL);
-	arr_size = count_words(trimmed, c);
-	splited = ft_calloc(sizeof(char *), (arr_size + 1));
-	if (!splited)
-		return (NULL);
-	str_arr = splited;
-	while (arr_size--)
-	{
-		*splited = trimmed;
-		while (*trimmed && *trimmed != c)
-			trimmed++;
-		while (*trimmed && *trimmed == c)
-		{
-			*trimmed = '\0';
-			trimmed++;
-		}
-		splited++;
-	}
-	splited = NULL;
-	return (str_arr);
+	return (split(strs, s, c));
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 19:58:40 by aaitelka          #+#    #+#             */
-/*   Updated: 2023/11/14 09:48:32 by aaitelka         ###   ########.fr       */
+/*   Updated: 2023/11/14 10:04:58 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	len;
 
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-	len = ft_strlen(s1);
-	while (ft_strchr(set, s1[start]))
+	len = ft_strlen(s1) - 1;
+	while (*s1 && ft_strchr(set, s1[start]))
 		start++;
-	if (start < len)
-		while (ft_strchr(set, s1[len - 1]))
+	if (start <= len)
+		while (ft_strchr(set, s1[len]))
 			len--;
-	str = ft_substr(s1, start, len - start);
+	str = ft_substr(s1, start, len - start + 1);
 	return (str);
 }
