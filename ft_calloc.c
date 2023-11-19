8838b8419abd7c34cf1b770bd548a7356e8c75b5
Halem <aaitelk@gmail.com>
2023-11-19 11:00:13 +0100

    libft finished.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:52:36 by aaitelka          #+#    #+#             */
-/*   Updated: 2023/11/10 15:58:50 by aaitelka         ###   ########.fr       */
+/*   Updated: 2023/11/19 10:54:41 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*s;
 
-    if (count > LONG_MAX && size > LONG_MAX)
-        return (NULL);
+	if (count > LONG_MAX && size > LONG_MAX)
+		return (NULL);
 	s = malloc(count * size);
 	if (!s)
 		return (NULL);
